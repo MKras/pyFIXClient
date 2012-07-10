fc4022eebd9627a6d77b433a2baf2901858a30cc
Mikhail Krasikau <Mikhail_Krasikau@epam.com>
2012-07-10 16:11:34 +0300

    test passed

--- tests.py
+++ tests.py
@@ -20,48 +20,21 @@ LOGGER = FIX_Log()
 
 hertbeat_interval = 0
 ###############################################################################################################################
-
-class Tester:
-  def __init__(self, tagClOrdID_11):
-    self.tagClOrdID_11 = tagClOrdID_11;
- 
-  def num(self, s):
-    try:
-      return int(s)
-    except exceptions.ValueError:
-      return float(s)
-
-  def push(self, msg):
-   if( fix.get_tag(msg,  11) == self.tagClOrdID_11):
-     print ('TEST = ', msg)
-   
-#######################   
-class Tester2(Tester):
-  def __init__(self, tagClOrdID_11):
-    self.tagClOrdID_11 = tagClOrdID_11;
- 
-  def num(self, s):
-    try:
-      return int(s)
-    except exceptions.ValueError:
-      return float(s)
-
-  def push(self, msg):
-   if( fix.get_tag(msg,  11) == self.tagClOrdID_11):
-     print ('TEST = ', msg)
-########################
 class Processor:
   def __init__(self):
+    self.test_passed = False
+    self.processed = False
+    self.finish = False
     pass
 
+    '''def __del__(self):
+    input("\nPress Enter to Logout...\n")
+    self.network_self.send(fix.generate_Logout_35_5())'''
+
   def process(self, msg, network_self = None):
-    tagClOrdID_11 = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(10))
     if (fix.get_tag(msg,  35) == '0'):
       msg = fix.generate_message( OrderedDict([ ('35',  '0'), ('49', sender), ('56' , target)]) )
     elif (fix.get_tag(msg,  35) == '8'):
-      #fix.set_LastOrderID_37(fix.get_tag(msg,  37))    
-      #print ('TAG 37 = '+fix.get_tag(msg,  37))
-      #print ('MSG WAS: '+msg)
       self.test(msg)
     elif (fix.get_tag(msg,  35) == '1'):
       reqId = fix.get_tag(msg,  112) 
@@ -78,21 +51,26 @@ class Processor:
     return msg
 
   def processor(self, msg, network_self = None):
-    input("\nPress Enter to continue...\n")        
-    for i in range (0, 1):
-      tagClOrdID_11 = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(10))
-      tagClOrdID_526 = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(5))
-      tagClOrdID_11_old = tagClOrdID_11
-      #msg = fix.generate_message( OrderedDict([ ('35',  'D'),('11', tagClOrdID_11), ('1','S01-00000F00'), ('38', 2),('40', 2), ('44', 76), ('54', 1), ('55', 'SBER'),   ('386', '1'), ('336', 'EQBR'), ('59', 0) ] ) )
-      msg = fix.generate_message( OrderedDict([ ('35',  'D'),('11', tagClOrdID_11), ('1','S01-00000F00'), ('38', 5),('40', 2), ('44', 42), ('54', 1), ('55', 'AFLT'), ('526',tagClOrdID_526 ),  ('386', '1'), ('336', 'EQBR'), ('59', 0) ] ) )
-      network_self.send(msg)
-    time.sleep(5)    
-    input("\nPress Enter to Logout...\n")
-    network_self.send(fix.generate_Logout_35_5())    
-    
+    self.network_self = network_self
+    #input("\nPress Enter to continue...\n")
+    self.tagClOrdID_11 = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(10))
+    self.tag_11 = self.tagClOrdID_11
+    self.tagClOrdID_526 = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(5))
+    self.tagClOrdID_11_old = self.tagClOrdID_11
+    msg = fix.generate_message( OrderedDict([ ('35',  'D'),('11', self.tagClOrdID_11), ('1','S01-00000F00'), ('38', 5),('40', 2), ('44', 42), ('54', 1), ('55', 'AFLT'), ('526',self.tagClOrdID_526 ),  ('386', '1'), ('336', 'EQBR'), ('59', 0) ] ) )
+    network_self.send(msg)
+    self.processed = True
+
+    def finish(self):
+      if(True == self.finish):
+        input("\nPress Enter to Logout...\n")
+        self.network_self.send(fix.generate_Logout_35_5())
     
   def test(self, msg):
     print('TEST msg = :',msg)
+    self.test_passed =  (fix.get_tag(msg, 11) == self.tag_11)
+    print('fix.get_tag(msg, 11) = ',fix.get_tag(msg, 11), ' self.tag_11 = ',self.tag_11)
+    self.finish = True
     pass
 
 
@@ -102,61 +80,15 @@ class TestSequenceFunctions(unittest.TestCase):
     self.process = self.processor.process 
     
     self.cl = Client(host, port, self.process)
-    #tests = Tester()
-    #self.cl.set_test_func(tests.push)
-    self.cl.send(logon_msg)
+    self.cl.send(logon_msg)    
     
   def test_1(self):
-    #tests = Tester();
-    #self.cl.set_test_func(tests.push)
-    self.assert_(1==1)
+    '''Test 1 failed'''
+    while(False == self.processor.processed):
+      print('test_1 waiting 5 sec')
+      time.sleep(5)
+    self.assert_(True == self.processor.test_passed)    
     
-
-
-
-
-##############################################################################################################################
-def process_trfix(msg, self = None, test_func = None):
-  tagClOrdID_11 = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(10))
-  #time.sleep(1)
-  if (fix.get_tag(msg,  35) == '0'):
-    msg = fix.generate_message( OrderedDict([ ('35',  '0'), ('49', sender), ('56' , target)]) )
-  elif (fix.get_tag(msg,  35) == '8'):
-    #fix.set_LastOrderID_37(fix.get_tag(msg,  37))    
-    #print ('TAG 37 = '+fix.get_tag(msg,  37))
-    print ('MSG WAS: '+msg)
-  elif (fix.get_tag(msg,  35) == '1'):
-    reqId = fix.get_tag(msg,  112) 
-    msg = fix.generate_message( OrderedDict([ ('35',  '0'), ('49', sender), ('56' , target), ('112', reqId)]) )
-  elif (fix.get_tag(msg,  35) == '5'):
-    msg = None
-  elif (fix.get_tag(msg,  35) == '4'):
-    fix.set_seqNum( fix.get_tag(msg,  36) )
-  elif (fix.get_tag(msg,  35) == 'A'):
-    tagClOrdID_11 = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(10))
-    #!!!! Simple Test Worked 35=D Request
-    input("\nPress Enter to continue...\n")    
-    for i in range (0, 1):      
-      tagClOrdID_526 = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(5))
-      tagClOrdID_11_old = tagClOrdID_11
-      #msg = fix.generate_message( OrderedDict([ ('35',  'D'),('11', tagClOrdID_11), ('1','S01-00000F00'), ('38', 2),('40', 2), ('44', 76), ('54', 1), ('55', 'SBER'),   ('386', '1'), ('336', 'EQBR'), ('59', 0) ] ) )
-      msg = fix.generate_message( OrderedDict([ ('35',  'D'),('11', tagClOrdID_11), ('1','S01-00000F00'), ('38', 5),('40', 2), ('44', 42), ('54', 1), ('55', 'AFLT'), ('526',tagClOrdID_526 ),  ('386', '1'), ('336', 'EQBR'), ('59', 0) ] ) )
-      self.send(msg)
-
-    time.sleep(5)    
-    input("\nPress Enter to Logout...\n")
-    self.send(fix.generate_Logout_35_5())
-
-    #time.sleep(30)
-    
-    #logout_msg =fix.generate_Logout_35_5()
-    #self.send(logout_msg)
-    msg=None
-  else:
-    msg = None
-  return msg
-##############################################################################################################################
-
 
 
 fix=FIX44()
