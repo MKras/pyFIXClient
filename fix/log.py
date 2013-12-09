c0c405ac0d1f3a60cda088159b0e9ee6e18339ae
Mikhail Krasikau <Mikhail_Krasikau@epam.com>
2013-12-09 13:55:10 +0300

    log output - do not open every time

--- fix/log.py
+++ fix/log.py
@@ -20,6 +20,9 @@ class FIX_Log(object):
         else:
             self.FIX_LOG_IN = FIX_Log.FIX_LOG_IN
             self.FIX_LOG_OUT = FIX_Log.FIX_LOG_OUT
+        
+        self.file_in = open(self.FIX_LOG_IN, encoding='utf-8',  mode='a')
+        self.file_out = open(self.FIX_LOG_OUT, encoding='utf-8',  mode='a')
     
     def log_in_msg(self,  msg):
         self.mutex.acquire()
@@ -29,28 +32,28 @@ class FIX_Log(object):
         msg = re.sub(r'8=FIX.4.4', r'\n8=FIX.4.4',msg )
         splitted_msg = msg.split('\n')
         if (not self.silent):
-          self.file = open(self.FIX_LOG_IN, encoding='utf-8',  mode='a')
           for msg in splitted_msg:
             if msg is not '':
               try:
                 if msg.index('8=FIX') < 0:
-                  self.file.write( msg )
+                  self.file_in.write( msg )
+                  self.file_in.flush()
                 else:
-                  self.file.write( '\n'+str(datetime.now()) +': '+msg )
+                  self.file_in.write( '\n'+str(datetime.now()) +': '+msg )
+                  self.file_in.flush()
               except ValueError as err:
-                self.file.write( msg )
-          self.file.close()
+                self.file_in.write( msg )
+                self.file_in.flush()
         
         self.mutex.release()
         return splitted_msg
-    
+
     def log_out_msg(self,  msg):
         self.mutex.acquire()
         #print('log_out_msg: '+msg)
         if (not self.silent):
-          self.file = open(self.FIX_LOG_OUT, encoding='utf-8',  mode='a')
-          self.file.write( str(datetime.now()) +': '+msg+'\n' )         
-          self.file.close()
+          self.file_out.write( str(datetime.now()) +': '+msg+'\n' )          
+          self.file_out.flush()
         self.mutex.release()
     
     def set_logs(self,  log_in=None,  log_out=None):
