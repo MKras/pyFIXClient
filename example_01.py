2327cf2baebd93e0111312b820f5797f774f81d8
Me <Mikhail_Krasikau@EPBYMINW2606.minsk.epam.com>
2016-05-12 13:59:21 +0300

    example 01 update

--- example_01.py
+++ example_01.py
@@ -18,9 +18,11 @@ def do_smth_01(msg, self):
   #!!!! Simple Test Worked 35=D Request
   #input("\nPress Enter to continue...\n")    
   order_new = OrderedDict([ ('35',  'D'),('11', self.fix.get_randomID()), ('1','S01-00000F00'), ('38', 10),('40', 2), ('44', 42), ('54', 1), ('55', 'AFLT'), ('526','' ),  ('386', '1'), ('336', 'EQBR'), ('59', 0) ] ) 
-  for i in range (0, 10):    
+  price = 1
+  for i in range (0, 10): 
+    price += 1
     tagClOrdID_526 = self.fix.get_randomID(5)    
-    order_new.update(OrderedDict([ ('11', self.fix.get_randomID()), ('526', tagClOrdID_526)]))
+    order_new.update(OrderedDict([ ('11', self.fix.get_randomID()), ('526', tagClOrdID_526),(('44', price))]))
     self.send(self.fix.generate_message(order_new))
     time.sleep(1)
   
